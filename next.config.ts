8223deefcff64d0482fc95341cc9db088535f674
Rilsosing <53212931+chimmykk@users.noreply.github.com>
2024-12-27 14:21:02 +0530

    disable linting
    disable linting

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next'
+ 
+const nextConfig: NextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 }
-
-module.exports = nextConfig
+ 
+export default nextConfig
