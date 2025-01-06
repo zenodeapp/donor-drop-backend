387df55bca2efb0cae481b1a878e9e9118a78683
ZENODE <zenodeapp@gmail.com>
2025-01-06 10:06:23 +0100

    Reintroduced isWithinDateRange and refactored variables

---
+++ _variables.mjs
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const ADDRESS = process.env.SCRAPER_ADDRESS;
+
+const START_BLOCK = process.env.SCRAPER_START_BLOCK || "0";
+
+const START_DATE = new Date(
+  process.env.SCRAPER_START_DATE || "2024-12-27T15:00:00Z"
+);
+
+const END_DATE = new Date(
+  process.env.SCRAPER_END_DATE || "2025-01-09T15:00:00Z"
+);
+
+export { ADDRESS, START_BLOCK, START_DATE, END_DATE };
