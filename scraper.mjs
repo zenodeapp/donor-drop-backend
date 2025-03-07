a934bc9cd7e770a2d625056a18a2a87a12072f0a
ZENODE <zenodeapp@gmail.com>
2025-03-07 18:53:02 +0100

    Added a --all-etherscan-txs flag instead of a bypass flag

--- scraper.mjs
+++ scraper.mjs
@@ -6,7 +6,7 @@ const PORT = process.env.SCRAPER_PORT || 3000;
 
 const args = process.argv.slice(2);
 const once = args.includes("--once");
-const bypassTnamValidation = args.includes("--bypass-tnam-validation"); // This will also consider 'once' to be set to 'true'
+const allEtherscanTxs = args.includes("--all-etherscan-txs"); // This will also consider 'once' to be set to 'true'
 
 createServer(async (req, res) => {
   res.statusCode = 200;
@@ -21,9 +21,9 @@ createServer(async (req, res) => {
   // Initialize server
   try {
     await initialize({
-      once: bypassTnamValidation || once,
-      bypassTnamValidation,
-      writeToEtherscanTable: bypassTnamValidation, // currently bypassTnamValidation will make sure we write to the etherscan_transactions_all table.
+      once: allEtherscanTxs || once,
+      allEtherscanTxs,
+      bypassTnamValidation: allEtherscanTxs, // currently allEtherscanTxs will make sure we write to the etherscan_transactions_all table.
     });
   } catch (error) {
     logError("Failed to initialize scraper:", error);
