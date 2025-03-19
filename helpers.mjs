fce4ae3c8472c1a919f0abec3c696c7d6967e24d
ZENODE <zenodeapp@gmail.com>
2025-03-19 07:00:24 +0100

    Fixes #31 and #32

--- helpers.mjs
+++ helpers.mjs
@@ -1,13 +1,59 @@
-const prettyTimestamp = () => (
-  new Date().toISOString().replace('T', ' ').split('.')[0]);
+import readline from "readline";
+
+const prettyTimestamp = () =>
+  new Date().toISOString().replace("T", " ").split(".")[0];
 
 const log = (...args) => {
   console.log(`[${prettyTimestamp()}]`, ...args);
-}
+};
 
 const logError = (...args) => {
   console.error(`[${prettyTimestamp()}]`, ...args);
-}
+};
 
+const askUser = (message, defaultYes = false, tabs = 0) => {
+  const indent = " ".repeat(tabs * 2);
 
-export { log, logError };
+  return new Promise((resolve) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    const ask = () => {
+      rl.question(
+        `${indent}${message} ${defaultYes ? "(Y/n)" : "(y/N)"} `,
+        (answer) => {
+          if (answer === "") {
+            rl.close();
+            resolve(defaultYes);
+          } else if (answer.toLowerCase() === "y") {
+            rl.close();
+            resolve(true);
+          } else if (answer.toLowerCase() === "n") {
+            rl.close();
+            resolve(false);
+          } else {
+            console.log(
+              `${indent}❌ Invalid input. Please enter 'y' for Yes or 'n' for No.`
+            );
+            ask(); // Ask again without closing
+          }
+        }
+      );
+    };
+
+    ask();
+  });
+};
+
+const createCsv = (columns, rows) => {
+  let result = columns.join(",") + "\n";
+  rows.forEach((row) => {
+    result += columns.map((col) => row[col] ?? "").join(",") + "\n";
+  });
+
+  return result;
+};
+
+export { log, logError, createCsv, askUser };
---
+++ result.mjs
@@ -0,0 +1,295 @@
+import { mkdir, rm, writeFile } from "fs/promises";
+import { pool } from "./lib/db.mjs";
+import { askUser, createCsv } from "./helpers.mjs";
+
+// Arguments
+// TODO: issue #15 will make the args --min-eth-per-address and --max-eth-per-address obsolete.
+const clean = process.argv.includes("--clean");
+const excludeEligibles = process.argv.includes("--exclude-eligibles");
+const excludeAboveCap = process.argv.includes("--exclude-above-cap");
+const excludeNotInDb = process.argv.includes("--exclude-not-in-db");
+const minEthPerAddress = process.argv.includes("--min-eth-per-address")
+  ? parseFloat(process.argv[process.argv.indexOf("--min-eth-per-address") + 1])
+  : 0.03;
+const maxEthPerAddress = process.argv.includes("--max-eth-per-address")
+  ? parseFloat(process.argv[process.argv.indexOf("--max-eth-per-address") + 1])
+  : 0.3;
+
+// Queries
+const queries = {};
+
+if (!excludeEligibles)
+  queries.eligibles = `SELECT from_address AS eth_address, tnam AS nam_address, eligible_amount AS eth_amount, suggested_nam AS nam_amount FROM private_result_eligible_addresses_finalized_in_db`;
+
+if (!excludeAboveCap)
+  queries.above_cap = `SELECT from_address AS eth_address, tnam AS nam_address, eligible_above_cap AS eth_amount, suggested_nam AS nam_amount FROM private_result_above_cap_addresses_in_db`;
+
+if (!excludeNotInDb)
+  queries.not_in_db = `SELECT from_address AS eth_address, tnam AS nam_address, total_eth AS eth_amount, suggested_nam AS nam_amount, sig_hash FROM private_result_addresses_not_in_db`;
+
+const main = async () => {
+  console.log();
+  console.log(`Arguments:`);
+  console.log(
+    `--exclude-eligibles: ${excludeEligibles.toString()} [exclude eligible users]`
+  );
+  console.log(
+    `--exclude-above-cap: ${excludeAboveCap.toString()} [exclude users who donated after the cap]`
+  );
+  console.log(
+    `--exclude-not-in-db: ${excludeNotInDb.toString()} [exclude users who were initially not included]`
+  );
+  console.log(
+    `--min-eth-per-address: ${minEthPerAddress.toString()} [default: 0.03]`
+  );
+  console.log(
+    `--max-eth-per-address: ${maxEthPerAddress.toString()} [default: 0.3]`
+  );
+  console.log(
+    `--clean: ${clean.toString()} [discards ./output before exporting results]`
+  );
+  console.log();
+
+  console.log(
+    `This will export the raw and final results in .csv, .json, and .proposal.json-format.`
+  );
+  const response = await askUser("Do you wish to continue?");
+
+  if (!response) {
+    console.log("Operation cancelled.");
+    process.exit(0); // Exit the script if the user does not confirm
+  }
+
+  console.log();
+
+  // Remove ./output folder if --clean argument is set
+  if (clean) {
+    try {
+      // Remove the entire 'output' folder and its contents
+      await rm("./output", { recursive: true, force: true });
+      console.log("🗑️   Removed ./output.");
+    } catch (e) {
+      console.error("🗑️   Error removing ./output :", error);
+    }
+  }
+
+  // Ensure ./output directory exists
+  await mkdir("./output", { recursive: true });
+
+  // Create raw exports and collect all entries for later use
+  console.log("💾  Saving raw results...");
+  const allRows = [];
+  await Promise.all(
+    Object.entries(queries).map(async ([key, query]) => {
+      const data = await createRawResults(query, `_raw_${key}`);
+
+      if (data) allRows.push(...data);
+    })
+  );
+
+  // Create final results
+  console.log("💾  Saving final results...");
+  const results = await createFinalResults(allRows);
+
+  showResults(results);
+
+  // Close DB connection
+  await pool.end();
+};
+
+const showResults = (data) => {
+  console.log();
+  console.log("🧮 Export finished successfully!");
+  console.log(`    👤 ${data.length} participants`);
+
+  let ethTotal = 0;
+  let namTotal = 0;
+
+  for (const row of data) {
+    ethTotal += parseFloat(row.eth_amount);
+    namTotal += row.nam_amount;
+  }
+  console.log(
+    `    💰 ${ethTotal.toFixed(6)} ETH donated (rounded by 6 decimals)`
+  );
+  console.log(`    👛 ${namTotal} NAM (rounded)`);
+};
+
+// Exports raw data and returns the rows as an object for later use
+const createRawResults = async (query, fileName) => {
+  try {
+    const result = await pool.query(query);
+
+    if (!result.rows.length) {
+      console.log(`No data found for ${fileName}.`);
+      return [];
+    }
+
+    const columns = result.fields.map((field) => field.name);
+
+    // raw .csv export
+    const csvFilePath = `./output/${fileName}.csv`;
+    await writeFile(csvFilePath, createCsv(columns, result.rows), "utf8");
+    console.log(`    Exported ${csvFilePath}.`);
+
+    // raw .json export
+    const jsonFilePath = `./output/${fileName}.json`;
+    await writeFile(jsonFilePath, JSON.stringify(result.rows, null, 2), "utf8");
+    console.log(`    Exported ${jsonFilePath}.`);
+
+    return result.rows;
+  } catch (error) {
+    console.error(`    Error fetching ${fileName}:`, error);
+    return [];
+  }
+};
+
+// Exports final results
+const createFinalResults = async (data) => {
+  // Helper function to validate whether participants are valid (tnam is accurate)
+  const validateParticipants = (_data) => {
+    const reducedData = _data.reduce((acc, row) => {
+      const { eth_address, eth_amount, nam_amount, sig_hash } = row;
+      const nam_address_lc = row.nam_address.toLowerCase();
+
+      // Check for missing nam_address
+      if (!nam_address_lc || sig_hash === null) {
+        console.log(`      ⏩  Skipped ${eth_address}`);
+        switch (true) {
+          case !nam_address_lc && sig_hash === null:
+            console.log(
+              `          Reason: missing tnam address and signature hash.`
+            );
+            break;
+          case !nam_address_lc:
+            console.log(`          Reason: missing tnam address.`);
+            break;
+          case sig_hash === null:
+            console.log(`          Reason: missing signature hash.`);
+            break;
+          default:
+        }
+
+        return acc; // Skip this row if nam_address is missing or sig hash is null
+      }
+
+      if (!acc[nam_address_lc]) {
+        acc[nam_address_lc] = {
+          nam_address: nam_address_lc,
+          eth_amount: 0,
+          nam_amount: 0,
+        };
+      }
+
+      acc[nam_address_lc].eth_amount += parseFloat(eth_amount);
+      acc[nam_address_lc].nam_amount += parseFloat(nam_amount);
+
+      return acc;
+    }, {});
+
+    return Object.values(reducedData);
+  };
+
+  // Helper function to (interactively) validate the eth amounts
+  const validateEthAmounts = async (_data) => {
+    for (const row of _data) {
+      const { nam_address } = row;
+
+      const fixedEthAmount = row.eth_amount.toFixed(6);
+      if (fixedEthAmount > maxEthPerAddress) {
+        console.log(`      ❔  About ${nam_address}`);
+        const response = await askUser(
+          `Issue: ${fixedEthAmount}E exceeds ${maxEthPerAddress}E. Would you like to cap this to ${maxEthPerAddress}E?`,
+          true,
+          5
+        );
+        if (response) {
+          console.log(
+            `          Solution: ✏️  Capped to ${maxEthPerAddress}E.`
+          );
+          row.nam_amount = Math.round(
+            (row.nam_amount / row.eth_amount) * maxEthPerAddress
+          );
+          row.eth_amount = maxEthPerAddress;
+          continue;
+        } else {
+          console.log(`          Solution: 🔹  Kept on ${row.eth_amount}E.`);
+        }
+      } else if (fixedEthAmount < minEthPerAddress) {
+        console.log(`      ❔  About ${nam_address}`);
+        const response = await askUser(
+          `Issue: ${fixedEthAmount}E is less than the min. required amount of ${minEthPerAddress}E. Would you like to exclude this participant?`,
+          false,
+          5
+        );
+        if (response) {
+          console.log(`          Solution: ❌  Excluded ${nam_address}.`);
+          // exclude this address from the array by first marking the address as null
+          row.nam_address = null;
+          continue;
+        } else {
+          console.log(`          Solution: 🔹  Kept ${nam_address}.`);
+        }
+      }
+
+      row.nam_amount = Math.round(row.nam_amount);
+      row.eth_amount = fixedEthAmount;
+    }
+
+    // Remove those addresses that are marked as not having donated enough
+    return _data.filter((row) => row.nam_address !== null);
+  };
+
+  if (!data.length) return;
+
+  const filteredRows = await validateEthAmounts(validateParticipants(data));
+
+  const fileName = `result_${[
+    !excludeEligibles ? "eligibles" : "",
+    !excludeAboveCap ? "above_cap" : "",
+    !excludeNotInDb ? "not_in_db" : "",
+  ]
+    .filter((str) => str !== "")
+    .join("+")}`;
+
+  // .csv export
+  const csvFilePath = `./output/${fileName}.csv`;
+  await writeFile(
+    csvFilePath,
+    createCsv(["nam_address", "eth_amount", "nam_amount"], filteredRows),
+    "utf8"
+  );
+  console.log(`    Exported ${csvFilePath}.`);
+
+  // .json export
+  const jsonFilePath = `./output/${fileName}.json`;
+  await writeFile(jsonFilePath, JSON.stringify(filteredRows, null, 2), "utf8");
+  console.log(`    Exported ${jsonFilePath}.`);
+
+  // .proposal.json export
+  const proposalJsonFilePath = `./output/${fileName}.proposal.json`;
+  await writeFile(
+    proposalJsonFilePath,
+    JSON.stringify(
+      filteredRows.map((row) => {
+        return {
+          Internal: {
+            amount: Math.floor(row.nam_amount * 1e6).toString(),
+            target: row.nam_address.toLowerCase().trim(),
+          },
+        };
+      }),
+      null,
+      2
+    ),
+    "utf8"
+  );
+  console.log(`    Exported ${proposalJsonFilePath}.`);
+
+  return filteredRows;
+};
+
+main().catch((error) => {
+  console.error("Error occurred:", error);
+  process.exit(1);
+});
